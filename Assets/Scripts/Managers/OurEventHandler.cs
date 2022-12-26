9df15d7394a70018963adb56bab1363f667c2c36
Yuval Lavie <yuval_lavie55@cs.huji.ac.il>
2022-12-26 18:18:01 +0200

    Game manager, adding and removing players

--- Assets/Scripts/Managers/OurEventHandler.cs
+++ Assets/Scripts/Managers/OurEventHandler.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class OurEventHandler : MonoBehaviour
 {
     private static int NumberOfPlayers = 2;
+    [SerializeField] private PlayerManager playerManager;
+    private int levelNumber = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +19,32 @@ public class OurEventHandler : MonoBehaviour
     void Update()
     {
         
+    }
+    public void AddPlayer()
+    {
+        if (NumberOfPlayers < 4)
+        {
+            NumberOfPlayers += 1;
+        }
+    }
+    public void RemovePlayer()
+    {
+        if (NumberOfPlayers > 2)
+        {
+            NumberOfPlayers -= 1;
+        }
+    }
+
+    public void Nextlevel(int numlevel = 0)
+    {
+        levelNumber += numlevel;
+        //playermanager.updateplayers(numberofplayers);
+        SceneManager.LoadScene("level " + levelNumber);
+        levelNumber += 1;
+    }
+
+    public int getNumOfPlayers()
+    {
+        return NumberOfPlayers;
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    [SerializeField] private OurEventHandler gameManager;
     private PlayerController[] players = new PlayerController[4];
     private int players_in_game = 2;
     // Start is called before the first frame update
     void Start()
     {
+        players_in_game = gameManager.getNumOfPlayers();
         int i = 0;
         foreach(PlayerController player in GetComponentsInChildren<PlayerController>())
         {
             players[i] = player;
+            if (players_in_game <= i)
+            {
+                player.gameObject.SetActive(false);
+            }
             i += 1;
-            player.gameObject.SetActive(false);
         }
     }
 
@@ -23,24 +28,25 @@ public class PlayerManager : MonoBehaviour
     {
         
     }
-    public void AddPlayer()
+    //public void AddPlayer()
+    //{
+    //    if (players_in_game < 4)
+    //    {
+    //        players[players_in_game].gameObject.SetActive(true);
+    //        players_in_game += 1;
+    //    }
+    //}
+    //public void RemovePlayer()
+    //{
+    //    if (players_in_game > 2)
+    //    {
+    //        players[players_in_game].gameObject.SetActive(false);
+    //        players_in_game -= 1;
+    //    }
+    //}
+    public void UpdatePlayers(int numPlayers)
     {
-        if (players_in_game < 4)
-        {
-            players[players_in_game].gameObject.SetActive(true);
-            players_in_game += 1;
-        }
-    }
-    public void RemovePlayer()
-    {
-        if (players_in_game > 2)
-        {
-            players[players_in_game].gameObject.SetActive(false);
-            players_in_game -= 1;
-        }
-    }
-    public void StartGame()
-    {
+        players_in_game = numPlayers;
         foreach(PlayerController player in players)
         {
             player.gameObject.SetActive(true);
