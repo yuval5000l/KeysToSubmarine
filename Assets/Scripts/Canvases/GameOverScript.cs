0b46e8fa13fc9f5816b442bf21bba55cd60e394a
Yuval Lavie <yuval5000l@gmail.com>
2023-01-12 20:11:38 +0200

    UpdatingEndGame

--- Assets/Scripts/Canvases/GameOverScript.cs
+++ Assets/Scripts/Canvases/GameOverScript.cs
@@ -7,11 +7,11 @@ public class GameOverScript : MonoBehaviour
 {
     //[SerializeField] private GameObject GameOverScreen;
     [SerializeField] private OurEventHandler GM;
-
+    private PlayerManager playerManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerManager = FindObjectOfType<PlayerManager>();
     }
 
     // Update is called once per frame
