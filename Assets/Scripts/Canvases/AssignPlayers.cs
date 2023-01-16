a9301215a24656338af5d99ca7bcddfa9e3e04f2
Yuval Lavie <yuval5000l@gmail.com>
2023-01-16 20:33:30 +0200

    Fooking Players SelectionScreen

--- Assets/Scripts/Canvases/AssignPlayers.cs
+++ Assets/Scripts/Canvases/AssignPlayers.cs
@@ -12,19 +12,20 @@ public class AssignPlayers : MonoBehaviour
     [SerializeField] private TMP_Text num_players_text;
     [SerializeField] private OurEventHandler gameManager;
     [SerializeField] private GameObject MainMenu;
-    [SerializeField] private SpriteRenderer player1;
-    [SerializeField] private SpriteRenderer player2;
-    [SerializeField] private SpriteRenderer player3;
+    [SerializeField] private SpriteRenderer[] players_animators = new SpriteRenderer[3];
     [SerializeField] private Button button;
+    
+    [SerializeField] List<InputDevice> players_devices = new List<InputDevice>();
+    List<int> players_devices_num = new List<int>();
+    List<int> player_num = new List<int>();
+    int[] player_num_default = new int[3] { 0, 1, 2 };
     private bool mainMenuActive = true;
     private int num_players = 0;
     private KeyCode player_1_action = KeyCode.Q;
     private KeyCode player_2_action = KeyCode.Space;
     private KeyCode player_3_action = KeyCode.B;
     private KeyCode player_4_action = KeyCode.M;
-    [SerializeField] private bool action_gamepad1 = false;
-    private bool action_gamepad2 = false;
-    private bool action_gamepad3 = false;
+    //[SerializeField] private bool action_gamepad1 = false;
     [SerializeField] private bool action_spacebar = false;
     [SerializeField] private bool action_B = false;
     [SerializeField] private bool action_Q = false;
@@ -58,83 +59,175 @@ public class AssignPlayers : MonoBehaviour
     //{
     //    myAction1.Enable();
     //}
+    private void AddPlayerAnimation()
+    {
+        Color goodColor = new Color(1f, 1f, 1f, 1f);
+        int cur_num = 0;
+        for (int i =0; i < 3; i++)
+        {
+            if (!player_num.Contains(i))
+            {
+                player_num.Add(i);
+                cur_num = i;
+                players_animators[cur_num].color = goodColor;
+                return;
+            }
+        }
+    }
+    private void RemovePlayerAnimation(int counter)
+    {
+        Color maColor = new Color(1f, 1f, 1f, 0.5f);
+        players_animators[player_num[counter]].color = maColor;
+        player_num.RemoveAt(counter);
 
-    private void AddPlayer(string name, string num)
+    }
+    private void AddPlayer(InputDevice inp, int num)
     {
         //gameManager
+        if (players_devices.Count < gameManager.getNumOfPlayers())
+        {
+            players_devices.Add(inp);
+            players_devices_num.Add(num);
+            AddPlayerAnimation();
+        }
+        
     }
-    private void RemovePlayer(string name, string num)
+    private void RemovePlayer(InputDevice inp, int num)
     {
+        if (players_devices.Count > 0)
+        {
+            int counter = -1;
+            for (int i = 0; i < players_devices.Count; i++)
+            {
+                if (players_devices[i] == InputSystem.GetDevice<InputDevice>() && players_devices_num[i] == num)
+                {
+                    counter = i;
+                }
+            }
 
+            players_devices.Remove(inp);
+            players_devices_num.Remove(num);
+            RemovePlayerAnimation(counter);
+
+        }
     }
     void OnKeyboard1(InputValue inp)
     {
-        if (inp.isPressed)
+        if (!inp.isPressed)
         {
-            if (action_Q)
+            int counter = -1;
+            for (int i = 0; i < players_devices.Count; i++)
             {
-                action_Q = false;
-                AddPlayer("keyboard", "2");
-
+                if (players_devices[i] == InputSystem.GetDevice<InputDevice>())
+                {
+                    if (players_devices_num[i] == 2)
+                    {
+                        counter = i;
+                    }
+                }
+            }
+            if (counter == -1)
+            {
+                AddPlayer(Keyboard.current, 2);
             }
             else
             {
-                action_Q = true;
-                RemovePlayer("keyboard", "2");
+                RemovePlayer(Keyboard.current, 2);
             }
         }
     }
     void OnKeyboard2(InputValue inp)
     {
-        if (inp.isPressed)
+        if (!inp.isPressed)
         {
-            if (action_spacebar)
+            int counter = -1;
+            for (int i = 0; i < players_devices.Count; i++)
             {
-                action_spacebar = false;
-                AddPlayer("keyboard", "1");
+                if (players_devices[i] == InputSystem.GetDevice<InputDevice>())
+                {
+                    if (players_devices_num[i] == 1)
+                    {
+                        counter = i;
+                    }
+                }
+            }
+            if (counter == -1)
+            {
+                AddPlayer(Keyboard.current, 1);
             }
             else
             {
-                action_spacebar = true;
-                RemovePlayer("keyboard", "1");
-
+                RemovePlayer(Keyboard.current, 1);
             }
         }
     }
     void OnKeyboard3(InputValue inp)
     {
-        if (inp.isPressed)
+        if (!inp.isPressed)
         {
-            if (action_B)
-            {
-                action_B = false;
-                AddPlayer("keyboard", "3");
-            }
-            else
-            {
-                action_B = true;
-                RemovePlayer("keyboard", "3");
+                int counter = -1;
+                for (int i = 0; i < players_devices.Count; i++)
+                {
+                    if (players_devices[i] == InputSystem.GetDevice<InputDevice>())
+                    {
+                        if (players_devices_num[i] == 3)
+                    {
+                            counter = i;
+                        }
 
-            }
+                    }
+                }
+                if (counter == -1)
+                {
+                    AddPlayer(Keyboard.current, 3);
+                }
+                else
+                {
+                    RemovePlayer(Keyboard.current, 3);
+                }
         }
     }
 
     void OnGamePad1(InputValue inp)
     {
-        if (inp.isPressed)
+        if (!inp.isPressed)
         {
-            if (action_gamepad1)
-            {
-                action_gamepad1 = false;
+            //Debug.Log(InputSystem.GetDevice<InputDevice>().ToString().Substring(InputSystem.GetDevice<InputDevice>().ToString().Length-1));
+            
+                if (!players_devices.Contains(InputSystem.GetDevice<InputDevice>()))
+                {
+                    if (players_devices.Count < gameManager.getNumOfPlayers())
+                    {
+                        players_devices.Add(InputSystem.GetDevice<InputDevice>());
+                        if (InputSystem.GetDevice<InputDevice>().ToString().Substring(InputSystem.GetDevice<InputDevice>().ToString().Length - 1) == "s")
+                        {
+                            players_devices_num.Add(0);
+                        }
+                        else
+                        {
+                            int x = int.Parse(InputSystem.GetDevice<InputDevice>().ToString().Substring(InputSystem.GetDevice<InputDevice>().ToString().Length - 1));
+                            players_devices_num.Add(x);
+                        }
+                        AddPlayerAnimation();
+                    }
+                }
+                else
+                {
+                    if (players_devices.Count > 0)
+                    {
+                        int counter = 0;
+                        for (int i = 0; i < players_devices.Count; i++)
+                        {
+                            if (players_devices[i] == InputSystem.GetDevice<InputDevice>())
+                            {
+                                counter = i;
+                            }
+                        }
+                        players_devices.Remove(InputSystem.GetDevice<InputDevice>());
+                        players_devices_num.RemoveAt(counter);
+                        RemovePlayerAnimation(counter);
+                }
             }
-            else
-            {
-                action_gamepad1 = true;
-            }
-        }
-        else
-        {
-            Debug.Log(InputSystem.GetDevice<InputDevice>());
         }
     }
     void Update()
@@ -149,7 +242,7 @@ public class AssignPlayers : MonoBehaviour
         //m_ButtonPressListener = InputSystem.onAnyButtonPress;
         //Debug.Log(m_ButtonPressListener);
         //Debug.Log(InputSystem.onAnyButtonPress.ToString());
-        if (num_players == gameManager.getNumOfPlayers())
+        if (players_devices.Count == gameManager.getNumOfPlayers())
         {
             button.enabled = true;
             button.image.color = new Color(1f, 1f, 1f, 1f);
