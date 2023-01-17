2dc2f8e9f5589fc0e7df05f19431bd1968e4bf79
Fidaanaffaa <fnagas@gmail.com>
2023-01-17 13:41:06 +0200

    Added level 1

---
+++ Assets/Scripts/Canvases/BorderScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderScript : MonoBehaviour
+{
+    private bool slideOut;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slideOut = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(slideOut)
+        {
+            gameObject.transform.localScale = gameObject.transform.localScale 
+            - new Vector3(0, Time.deltaTime, 0);
+        }
+        if(gameObject.transform.localScale.y <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SlideOut()
+    {
+        slideOut = true;
+    }
+}
